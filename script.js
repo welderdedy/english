dc2da8312fdfb74c7fd5a11507fe3627af76a455
feet <weldersilvasurf@gmail.com>
2022-06-29 16:14:48 -0300

    english

---
+++ script.js
@@ -0,0 +1,102 @@
+let comedy = ["the white ones","low blow","debi and loidi"];
+console.log(comedy);
+
+let dice = [ 
+
+    {
+        title:"the white ones",
+        director :"Keenen Ivory Wayans", 
+        year: 2004
+    },
+
+
+
+
+    {
+        title:"low blow",
+        director :"Peter Segal", 
+        year: 2005
+    },
+
+
+
+
+    {
+        title:"debi and loidi",
+        director :"Peter Farrely", 
+        year: 1994
+    }
+
+]
+
+console.log(dice[0]);
+console.log(dice[1]);
+console.log(dice[2]);
+   
+               
+
+let action = ["007 casino royale","mission impossible folout effect ","the avengers ultimatum"]
+console.log(action);
+
+let sumary = [
+   {
+     title : "007 casino royale",
+     director : "Martin Campbel",
+     year : 2006 
+
+ },
+
+  {
+    title : "mission impossible folout effect",
+    director : "Cristhofer Mcquarrie",
+    year : 2018 
+ },
+
+ {
+    title : "the avengers ultimatum",
+    director : "Anthony Russo",
+    year : 2019 
+ }
+ 
+]
+
+console.log(sumary[0]);
+console.log(sumary[1]);
+console.log(sumary[2]);
+
+
+let fantasy = ["the lord of the rings the two towers","the lord of the rings the return of the king ","the hobbit the desolation of smaug"]
+console.log(fantasy);
+
+let figure = [
+    {
+        title : "the lord of the rings the two towers",
+        director : "Peter Jackson",
+        year : 2002   
+    },
+
+    {
+        title : "the lord of the rings the return of the king",
+        director : "Peter Jackson",
+        year : 2003   
+    },
+
+    {
+        title : "the hobbit the desolation of smaug",
+        director : "Peter Jackson",
+        year : 2013   
+    },
+]
+
+console.log(figure[0]);
+console.log(figure[1]);
+console.log(figure[2]);
+
+let movie = "the avengers ultimato";
+
+if(movie == "the avengers ultimato" || movie == "x men" ){
+    console.log("MY FAVORIT MOVIE IS ULTIMATO");
+}
+else{
+    console.log("NOT EVEN 5 STARS");
+}
